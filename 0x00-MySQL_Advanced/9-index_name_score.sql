3797df243d51350b7e205ed3ae7c85383e8b83fc
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-01-18 12:46:46 -0800

    Corrected Task9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,4 @@
 -- Create an index idx_name_first_score on the table names
 
-CREATE INDEX idx_name_first_score ON names((name, 1), (score, 1));
+CREATE INDEX idx_name_first_score
+ON names(name(1), score);
